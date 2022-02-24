1551a9b57a16f3efe59ea7428d3c817ebb4710db
harshvardhan2707 <72443636+harshvardhan2707@users.noreply.github.com>
2022-02-24 23:17:41 +0530

    Removed more unneccesary files

--- Daksh.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-#define MULTIPLY(a, b) a*b
-
-int main(){
-    cout << MULTIPLY((2+4), (5+15));
-    return 0;
-}
--- input.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int arr[5]={10,20,30,40,50};
-    cout<<arr[2];
-}
